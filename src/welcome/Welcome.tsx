55fa5629f79391840568506287ab77e67ee7b224
Arul George <arulthayalan@hotmail.com>
2019-07-28 23:30:04 +1000

    emotion style compomnent

--- src/welcome/Welcome.tsx
+++ src/welcome/Welcome.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import WelcomeProps from './types';
+import { StyledH1, StyledButton } from './styled';
 
 export default class Welcome extends React.Component<WelcomeProps, {}> {
     public render() {
         const { name }: WelcomeProps = this.props;
 
-        return <div> Hello {name} welcome to react type script template </div>;
+        return (
+            <div>
+                <StyledH1> Hello {name} welcome to react type script template </StyledH1>
+                <StyledButton>Styled button </StyledButton>
+            </div>
+        );
     }
 }
---
+++ src/welcome/styled.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const StyledH1 = styled('h1')`
+    color: blue;
+    font-family: 'Arial';
+`;
+
+export const StyledButton = styled('button')`
+    color: black;
+    background-color: gray;
+    border-radius: 12px;
+    font-size: 16px;
+    margin: 4px 2px;
+    padding: 8px;
+`;
